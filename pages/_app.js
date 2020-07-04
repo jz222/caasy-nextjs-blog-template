16b11642756d73a9bae64c93a0ebe566baa9210d
Timo <timotheus.zimmermann@me.com>
2020-07-04 12:42:36 +0200

    add loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import NProgress from 'nprogress';
 import caasy from '@caasy/sdk-js';
+import Router from 'next/router';
 
+// Import style sheets
 import '../stylesheets/global.css';
+import '../stylesheets/nprogress.css';
 
-caasy.init({ mode: 'dev', siteId: '5ee4debb36189554686eb89b'});
+// Initialize the Caasy SDK
+caasy.init({ mode: 'dev', siteId: '5ee4debb36189554686eb89b' });
+
+// Loading indicator
+Router.events.on('routeChangeStart', NProgress.start);
+Router.events.on('routeChangeComplete', NProgress.done);
+Router.events.on('routeChangeError', NProgress.done);
 
 const App = ({ Component, pageProps }) => (
     <Component {...pageProps} />

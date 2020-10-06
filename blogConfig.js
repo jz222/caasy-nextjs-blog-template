645b132ad511c0cfdc0b207fc9b599b6c113cdda
Timo <timotheus.zimmermann@me.com>
2020-10-06 21:26:17 +0200

    update id

--- blogConfig.js
+++ blogConfig.js
@@ -8,7 +8,7 @@
  */
 export default {
     caasySDKConfig: {
-        siteId: '5f05b799985d5565e1826922'
+        siteId: '5f7cc20dc302c4fa0a961f8f'
     },
     blogName: 'myBlog',
     defaultTitle: 'my personal tech blog',
